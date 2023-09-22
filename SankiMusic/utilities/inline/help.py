28140991f86d597298fca6b2121efba6ce9a1acc
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-09-22 19:16:03 +0530

    Update help.py

--- SankiMusic/utilities/inline/help.py
+++ SankiMusic/utilities/inline/help.py
@@ -16,7 +16,7 @@ def help_pannel(_, START: Union[bool, int] = None):
         ),
         InlineKeyboardButton(
             text="ᴍᴏʀᴇ",
-            url=f"t.me/iamkaal",
+            url=f"t.me/about_ranavsnshi_divy",
         ),
         InlineKeyboardButton(
             text="ᴄʟᴏsᴇ", callback_data=f"close"
